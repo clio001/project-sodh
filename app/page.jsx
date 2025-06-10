39fd7207a0f35f582e80e65738e8b30fd88b9799
clio001 <johnwoitkowitz@hotmail.com>
2025-06-10 23:57:49 +0200

    installed MUI

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+"use client";
+import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
+
 export default function Home() {
+
+  const [number, setNumber] = useState(1);
+
   return (
-    <main >
-      <h1>Project SODH</h1>
-    </main>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '90vh' }}>
+      <Typography variant="h5" mb={3}>PROJECT SODH</Typography>
+      <Typography mb={1}>{number}</Typography>
+      <Button variant="contained" onClick={() => setNumber(number + 1)}>Click</Button>
+      <Typography  mt={5}>A world of delicacies awaits ...</Typography>
+    </Box>
   )
 }
