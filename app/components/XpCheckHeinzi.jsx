fcca958b74ffbc8ee03f1520a962cadfaa7b4b70
clio001 <johnwoitkowitz@hotmail.com>
2025-08-01 09:18:45 +0200

    raised XP to 60 for  HeinziBlurb

--- app/components/XpCheckHeinzi.jsx
+++ app/components/XpCheckHeinzi.jsx
@@ -7,7 +7,7 @@ function XpCheckHeinzi() {
 
   return (
     <div>
-      {player.xp >= 50 ? <HeinziBlurb badge={inventoryBadges[0]} /> : null}
+      {player.xp >= 60 ? <HeinziBlurb badge={inventoryBadges[0]} /> : null}
       {player.inventory.items.length == 2 ? (
         <HeinziBlurb badge={inventoryBadges[1]} />
       ) : null}
