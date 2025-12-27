20596dd5974747822821d0e2fd0657b06738d7e1
clio001 <johnwoitkowitz@hotmail.com>
2025-12-27 13:05:06 +0100

    added menu jsons and context variables

--- app/components/ContextProvider.jsx
+++ app/components/ContextProvider.jsx
@@ -6,6 +6,8 @@ import DBitems from "../../public/dbs/db-items.json";
 import DBjentries from "../../public/dbs/db-journal.json";
 import DBscenes from "../../public/dbs/db-scenes.json";
 import DBbadges from "../../public/dbs/db-badges.json";
+import DBmenu from "../../public/dbs/db-menu.json";
+import DBmenuEN from "../../public/dbs/db-menuEN.json";
 const MyContextProvider = createContext();
 
 export const ContextProvider = ({ children }) => {
@@ -27,6 +29,7 @@ export const ContextProvider = ({ children }) => {
   const [inventoryEntries, setInventoryEntries] = useState(DBjentries);
   const [scene, setScene] = useState(DBscenes);
   const [inventoryBadges, setInventoryBadges] = useState(DBbadges);
+  const [menu, setMenu] = useState(DBmenu);
 
   const [flash, setFlash] = useState("schreibtisch");
   return (
@@ -46,6 +49,8 @@ export const ContextProvider = ({ children }) => {
         setInventoryBadges,
         flash,
         setFlash,
+        menu,
+        setMenu,
       }}
     >
       {children}
